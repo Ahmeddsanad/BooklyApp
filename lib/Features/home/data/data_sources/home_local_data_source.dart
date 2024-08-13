8c13a8aed6e5470b8b6171d4f9d502f39565c805
Ahmed Sanad <ahmed.hac26@gmail.com>
2024-08-13 04:49:30 +0300

    fetch featured books home repo implementation

--- lib/Features/home/data/data_sources/home_local_data_source.dart
+++ lib/Features/home/data/data_sources/home_local_data_source.dart
@@ -2,12 +2,12 @@ import 'package:bookly/Features/home/domain/entites/book_entity.dart';
 import 'package:bookly/constants.dart';
 import 'package:hive/hive.dart';
 
-abstract class HomeRemoteDataSource {
+abstract class HomeLocalDataSource {
   List<BookEntity> fetchFeaturedBooks();
   List<BookEntity> fetchNewestBooks();
 }
 
-class HomeLocalDataSourceimpl extends HomeRemoteDataSource {
+class HomeLocalDataSourceimpl extends HomeLocalDataSource {
   @override
   List<BookEntity> fetchFeaturedBooks() {
     var books = Hive.box<BookEntity>(kFeaturedBox);
